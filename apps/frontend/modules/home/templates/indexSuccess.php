a47c4b273c1ec9daf24e5b15618edb9cf4ec2a2b
elio <elio@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-04-16 17:26:35 +0000

    ELIO: add frontend files

---
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -0,0 +1,103 @@
+<div class="left">
+	<div class="buscador">
+		<div class="titulo"></div>
+		<div class="search_box clearfix">
+			<form action="" method="">
+				<div class="rowElem">
+					<select name="select2" >
+						<option value="opt1">Propiedad</option>
+						<option value="opt2">Casa</option>
+						<option value="opt3">Departamento</option>
+						<option value="opt4">Duplex</option>
+					</select>
+				</div>
+				<div class="rowElem">
+					<select name="select2" >
+						<option value="opt1">Operacion</option>
+						<option value="opt2">Casa</option>
+						<option value="opt3">Departamento</option>
+						<option value="opt4">Duplex</option>
+					</select>
+				</div>
+				<div class="rowElem">
+					<select name="select2" >
+						<option value="opt1">Lugar</option>
+						<option value="opt2">Casa</option>
+						<option value="opt3">Departamento</option>
+						<option value="opt4">Duplex</option>
+					</select>
+				</div>
+				<div class="rowElem">
+					<select name="select2" >
+						<option value="opt1">Localidad</option>
+						<option value="opt2">Casa</option>
+						<option value="opt3">Departamento</option>
+						<option value="opt4">Duplex</option>
+					</select>
+				</div>
+				<div class="rowElem">
+					<select name="select2" >
+						<option value="opt1">Barrio</option>
+						<option value="opt2">Casa</option>
+						<option value="opt3">Departamento</option>
+						<option value="opt4">Duplex</option>
+					</select>
+				</div>
+				<div class="rowElem">
+					<select name="select2" >
+						<option value="opt1">Precio</option>
+						<option value="opt2">Casa</option>
+						<option value="opt3">Departamento</option>
+						<option value="opt4">Duplex</option>
+					</select>
+				</div>	
+				<div class="rowElem">
+					<select name="select2" >
+						<option value="opt1">Dormitorios</option>
+						<option value="opt2">Casa</option>
+						<option value="opt3">Departamento</option>
+						<option value="opt4">Duplex</option>
+					</select>
+				</div>
+			</form>
+		</div>
+		<div class="boton"><a href="#" class="buscar"></a></div>
+	</div>
+</div>
+<!-- -->
+<div class="right">
+	<div class="box clearfix">
+		<div class="inner clearfix">
+			<div class="titulo"><img src="/images/tit_tasa.png" alt="Tasá tu propiedad" /></div>
+			<p>
+				¿Querés vender o alquilar tu casa?<br />
+				Te brindamos la seguridad de un <strong>Perito Tasador Oficial.</strong>
+			</p>
+			<div class="boton">
+				<a href="#" class="vende"></a>
+				<a href="#" class="alquila"></a>
+			</div>
+		</div>
+	</div>
+	<div class="sombra"></div>
+	<div class="box clearfix">
+		<div class="inner clearfix">
+			<div class="titulo"><img src="/images/tit_creaperfil.png" alt="Creá tu perfil" /></div>
+			<p>Creá tu perfil de preferencias y <strong>las propiedades te encuentran a vos!</strong></p>
+			<div class="boton">
+				<a href="#" class="ingresar"></a>
+			</div>
+		</div>
+	</div>
+	<div class="sombra"></div>
+	<div class="box clearfix">
+		<div class="inner clearfix">
+			<div class="titulo"><img src="/images/tit_pertenece.png" alt="Pertenecé a la comunidad" /></div>
+			<p>Enviá tu <strong>CV</strong> o tu perfil de <strong>Facebook</strong>.</p>
+			<div class="boton">
+				<a href="#" class="enviar"></a>
+			</div>
+		</div>
+	</div>
+	<div class="sombra"></div>
+</div>
---
+++ apps/frontend/modules/search/actions/actions.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * search actions.
+ *
+ * @package    ilamarca
+ * @subpackage search
+ * @author     pinika
+ * @version    1
+ */
+class searchActions extends sfActions
+{
+  /**
+   * Executes index action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeIndex(sfWebRequest $request)
+  {
+    
+  }
+
+} // end class
