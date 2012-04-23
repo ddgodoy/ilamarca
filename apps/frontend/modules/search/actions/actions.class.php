6e5b04491e80a786e8f922708e01b6ac25028700
mauro2 <mauro2@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-04-23 15:08:46 +0000

    Update query frontend search property

--- apps/frontend/modules/search/actions/actions.class.php
+++ apps/frontend/modules/search/actions/actions.class.php
@@ -17,8 +17,14 @@ class searchActions extends sfActions
    */
   public function executeIndex(sfWebRequest $request)
   {
+        $this->p_desde       = $this->getRequestParameter('p_from', 0);
+        $this->p_hasta       = $this->getRequestParameter('p_to', 0);
+        $this->currency      = $this->getRequestParameter('currency', 0);
+
+        $array_data_currency = array('currency'=>$this->currency, 'p_desde'=>$this->p_desde, 'p_hasta'=>$this->p_hasta);
+
   	$this->iPage  = $request->getParameter('page', 1);
-  	$this->oPager = RealPropertyTable::getInstance()->searchResults($this->iPage, 12, $this->setFilter());
+  	$this->oPager = RealPropertyTable::getInstance()->searchResults($this->iPage, 12, $this->setFilter(),'',$array_data_currency);
   	$this->oList  = $this->oPager->getResults();
   }
   
@@ -33,36 +39,33 @@ class searchActions extends sfActions
   	$this->f_params = '';
 
   	$this->property_type = $this->getRequestParameter('property_type', 0);
-    $this->operation     = $this->getRequestParameter('operation', 0);
-    $this->geo_zone      = $this->getRequestParameter('geo_zone', 0);
-    $this->city          = $this->getRequestParameter('property_type', 0);
-    $this->neighborhood  = $this->getRequestParameter('neighborhood', 0);
-    $this->p_desde       = $this->getRequestParameter('p_from', 0);
-    $this->p_hasta       = $this->getRequestParameter('p_to', 0);
-    $this->currency      = $this->getRequestParameter('currency', 0);
-    $this->bedroom       = $this->getRequestParameter('bedroom', 0);
+        $this->operation     = $this->getRequestParameter('operation', 0);
+        $this->geo_zone      = $this->getRequestParameter('geo_zone', 0);
+        $this->city          = $this->getRequestParameter('property_type', 0);
+        $this->neighborhood  = $this->getRequestParameter('neighborhood', 0);
+        $this->bedroom       = $this->getRequestParameter('bedroom', 0);
 
-		if (!empty($this->property_type)) {
-			$sch_partial .= " AND p.property_type_id = $this->property_type";
-			$this->f_params .= '&property_type='.$this->property_type;
-		}
-		if (!empty($this->geo_zone)) {
-			$sch_partial .= " AND p.geo_zone_id = $this->geo_zone";
-			$this->f_params .= '&geo_zone='.$this->geo_zone;
-		}
-		if (!empty($this->city)) {
-			$sch_partial .= " AND p.city_id = $this->city";
-			$this->f_params .= '&city='.$this->city;
-		}
-		if (!empty($this->neighborhood)) {
-			$sch_partial .= " AND p.neighborhood_id = $this->neighborhood";
-			$this->f_params .= '&neighborhood='.$this->neighborhood;
-		}
-		if (!empty($this->bedroom)) {
-			$sch_partial .= " AND p.bedroom_id = $this->bedroom";
-			$this->f_params .= '&bedroom='.$this->bedroom;
-		}
-		return $sch_partial;
+        if (!empty($this->property_type)) {
+                $sch_partial .= " AND p.property_type_id = $this->property_type";
+                $this->f_params .= '&property_type='.$this->property_type;
+        }
+        if (!empty($this->geo_zone)) {
+                $sch_partial .= " AND p.geo_zone_id = $this->geo_zone";
+                $this->f_params .= '&geo_zone='.$this->geo_zone;
+        }
+        if (!empty($this->city)) {
+                $sch_partial .= " AND p.city_id = $this->city";
+                $this->f_params .= '&city='.$this->city;
+        }
+        if (!empty($this->neighborhood)) {
+                $sch_partial .= " AND p.neighborhood_id = $this->neighborhood";
+                $this->f_params .= '&neighborhood='.$this->neighborhood;
+        }
+        if (!empty($this->bedroom)) {
+                $sch_partial .= " AND p.bedroom_id = $this->bedroom";
+                $this->f_params .= '&bedroom='.$this->bedroom;
+        }
+        return $sch_partial;
   }
 
 } // end class
