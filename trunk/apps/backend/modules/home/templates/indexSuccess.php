efc6356252b8f5ec5815dd9448f4a95946f2cc31
elio <elio@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-05-14 12:08:53 +0000

    ELIO: upd modelo y ABM propiedades

--- trunk/apps/backend/modules/home/templates/indexSuccess.php
+++ trunk/apps/backend/modules/home/templates/indexSuccess.php
@@ -1,16 +1,7 @@
-<div class="content">
-	<div class="columnas col3">
-		<div class="paneles">
-			<h1>
-				Herramientas <a href="#" class="cerrar">Cerrar Panel</a>
-			</h1>
-			<ul class="herramientas">
-				<li><a href="#" class="view"> View pending orders</a></li>
-				<li><a href="#" class="newslet"> Create newsletter content</a></li>
-				<li><a href="#" class="add"> Add new product</a></li>
-				<li><a href="#" class="add"> Add new designer</a></li>
-			</ul>
-		</div>
+<div class="content" style="text-align:center;">
+	<div>
+		<br /><br />
+		<img src="/admin/images/home.png" width="150" border="0" />
 	</div>
 </div>
 <div class="clear"></div>
