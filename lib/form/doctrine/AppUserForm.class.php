cf34b85239fa4884a28cbd2b2c987cdb0c7114bb
mauro2 <mauro2@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-05-21 16:51:38 +0000

    Update Frontend insert User

--- lib/form/doctrine/AppUserForm.class.php
+++ lib/form/doctrine/AppUserForm.class.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * AppUser form.
  *
@@ -12,5 +11,46 @@ class AppUserForm extends BaseAppUserForm
 {
   public function configure()
   {
+      $user_name = '';
+
+      $this->setWidgets(array(
+      'id'            => new sfWidgetFormInputHidden(),
+      'name'          => new sfWidgetFormInputText(),
+      'last_name'     => new sfWidgetFormInputText(),
+      'email'         => new sfWidgetFormInputText(),
+      're_email'      => new sfWidgetFormInputText(),
+      'photo'         => new sfWidgetFormInputText(),
+      'password'      => new sfWidgetFormInputPassword(),
+      're_password'   => new sfWidgetFormInputPassword(),
+      'captcha'       => new sfWidgetFormReCaptcha(array('public_key' => sfConfig::get('app_recaptcha_public_key'))),
+    ));
+
+    $this->setValidators(array(
+      'id'            => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'name'          => new sfValidatorString(array('max_length' => 100, 'required' => true)),
+      'last_name'     => new sfValidatorString(array('max_length' => 100, 'required' => true)),
+      'email'         => new sfValidatorEmail(array(),array('invalid'=>'<em>Introduce un E-mail válido</em>', 'required' => '<em>Este campo es obligatorio</em>')),
+      're_email'      => new sfValidatorString(array('max_length' => 100, 'required' => true), array('required' => '<em>Este campo es obligatorio</em>','max_length'=>'<em>E-mail no valido</em>')),
+      'photo'         => new sfValidatorString(array('max_length' => 50, 'required' => false)),
+      'password'      => new sfValidatorString(array('max_length' => 100, 'required' => true), array('required' => '<em>Este campo es obligatorio</em>','max_length'=>'<em>Contraseña no valida</em>')),
+      're_password'   => new sfValidatorString(array('max_length' => 100, 'required' => true), array('required' => '<em>Este campo es obligatorio</em>','max_length'=>'<em>Contraseña no valida</em>')),
+      'captcha'       => new sfValidatorReCaptcha(array('private_key' => sfConfig::get('app_recaptcha_private_key')),array('captcha'=>true)),
+    ));
+
+    // validator for user login
+
+    $v = new sfValidatorSchemaCompare('user_name', sfValidatorSchemaCompare::EQUAL, 're_user_name', array(), array('invalid' =>'<em>Los E-mails no son iguales</em>'));
+    if($user_name && $this->getObject()->isNew())
+    {
+        $v = new sfValidatorSchemaCompare('user_name', sfValidatorSchemaCompare::EQUAL, $user_name, array(), array('invalid'=>'<em>El E-mail ya está registrado</em>'));
+    }
+
+    $this->validatorSchema->setPostValidator(
+       new sfValidatorAnd(
+       array($v,
+             new sfValidatorSchemaCompare('password', sfValidatorSchemaCompare::EQUAL, 're_password', array(), array('invalid' => '<em> Las contraseñas no son iguales</em>'))
+       )));
+
+    $this->widgetSchema->setNameFormat('app_user[%s]');
   }
 }
