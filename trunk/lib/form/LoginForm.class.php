70798ef6b89054a63cc7916f48a5c2b71c89aa6e
mauro2 <mauro2@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-05-24 12:23:05 +0000

    Update Frontend User

---
+++ trunk/lib/form/LoginForm.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Login form
+ *
+ * @package    muevetumovil
+ * @author     Keyvan Akbary <keyvan@samarcoweb.com>
+ */
+class LoginForm extends BaseForm
+{
+    public function configure()
+    {
+        $this->setWidgets(array(
+            'email' => new sfWidgetFormInput(),
+            'password' => new sfWidgetFormInput(array('type' => 'password'))
+        ));
+
+        $this->setValidators(array(
+            'email' => new sfValidatorString(),
+            'password' => new sfValidatorString()
+        ));
+
+        $this->validatorSchema->setPostValidator(new ValidatorUser());
+
+        $this->widgetSchema->setNameFormat('login[%s]');
+    }
+}
