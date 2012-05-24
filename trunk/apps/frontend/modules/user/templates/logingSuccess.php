5a0c54f2388b3d4e84edd11a73c83a6db495dcde
mauro2 <mauro2@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-05-24 17:45:12 +0000

    Update Frontend User

--- trunk/apps/frontend/modules/user/templates/logingSuccess.php
+++ trunk/apps/frontend/modules/user/templates/logingSuccess.php
@@ -11,12 +11,15 @@
 	<div class="contacto">
 		<div class="search_box clearfix" style="padding-top:10px;">
 			<form action="<?php url_for('home/contact') ?>" method="post">
+                                <p><strong>Si ya creaste tu perfil ingresa tu datos!</strong></p>
+                                <br/>
 				<div class="rowElem">
 					<p><strong><?php echo $form['email']->renderLabel('* Email:') ?></strong></p>
 					<?php
 						$error_email = $form['email']->renderError() ? 'background-color:#FFCCCC;' : '';
 						echo $form['email']->render(array('class'=>'et_input','style'=>'width:255px;'.$error_email));
 					?>
+                                        <?php if ($form['email']->renderError()): ?><p class="p-error"><em><?php echo '*'.strip_tags($form['email']->renderError()) ?></em></p><?php endif; ?>
 				</div>
 				<div class="rowElem">
 					<p><strong><?php echo $form['password']->renderLabel('* Contraseña:') ?></strong></p>
@@ -24,32 +27,26 @@
 						$error_password = $form['password']->renderError() ? 'background-color:#FFCCCC;' : '';
 						echo $form['password']->render(array('class'=>'et_input','style'=>'width:255px;'.$error_password));
 					?>
+                                        <?php if ($form['password']->renderError()): ?><p class="p-error"><em><?php echo '*'.strip_tags($form['password']->renderError()) ?></em></p><?php endif; ?>
 				</div>
 				<?php echo $form->renderHiddenFields() ?>
-				<p <?php if ($form->hasErrors()): ?>style="color:red;"<?php endif; ?>>Los campos con asterisco (*) son obligatorios</p>
-				<div class="boton" style="bottom: 20px;">
+				<p>Los campos con asterisco (*) son obligatorios</p>
+				<div class="boton" style="top: 200px;">
 					<input type="submit" value="" class="et_btn_buscar"/>
 				</div>
 			</form>
+                        <br clear="all"/>
+                        <div style=" margin-top: 80px">
+                        <p>¿Ha olvidado la información de acceso? <a href="" class="a-intro">Click Aquí</a></p>
+                        </div>
 		</div>
 		<div class="divider"></div>
-		<div class="info">
-			<p>
-				<strong>Inmobiliaria Lamarca</strong><br />
-				Av. Lamarca 3920 - Barrio Urca<br />
-				Córdoba (5009) Argentina. <br />
-				Tel/Fax: 0351- 4824473
-			</p>
-			<p>
-				<strong>Matías</strong><br />
-				Cel: 0351- 153290796 <br />
-				E-mail: <a href="mailto:matias@inmobiliarialamarca.com">matias@inmobiliarialamarca.com</a>
-			</p>
-			<p>
-				<strong>Luciana</strong><br />
-				Cel: 0351- 153290794 <br />
-				E-mail: <a href="mailto:luciana@inmobiliarialamarca.com">luciana@inmobiliarialamarca.com</a>
-			</p>
+		<div class="info register">
+                    <p><strong>Regístrate</strong></p>
+                    <p>Si todavía no creaste tu perfil ingresa aquí!</p>
+                    <div class="boton" style="top: 120px;">
+                        <input type="button" value="" onclick="document.location='<?php echo url_for('user/index') ?>';" class="et_btn_buscar"/>
+                    </div>
 		</div>
 	</div>
 </div>
