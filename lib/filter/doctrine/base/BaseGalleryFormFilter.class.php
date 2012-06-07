e26854cae806b824d0be987f4ab805517922d4a7
mauro2 <mauro2@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-06-07 18:02:20 +0000

    Update Outstanding Gallery

--- lib/filter/doctrine/base/BaseGalleryFormFilter.class.php
+++ lib/filter/doctrine/base/BaseGalleryFormFilter.class.php
@@ -15,6 +15,7 @@ abstract class BaseGalleryFormFilter extends BaseFormFilterDoctrine
     $this->setWidgets(array(
       'former_name'      => new sfWidgetFormFilterInput(),
       'internal_name'    => new sfWidgetFormFilterInput(),
+      'outstanding'      => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
       'real_property_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('RealProperty'), 'add_empty' => true)),
       'created_at'       => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
       'updated_at'       => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
@@ -23,6 +24,7 @@ abstract class BaseGalleryFormFilter extends BaseFormFilterDoctrine
     $this->setValidators(array(
       'former_name'      => new sfValidatorPass(array('required' => false)),
       'internal_name'    => new sfValidatorPass(array('required' => false)),
+      'outstanding'      => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
       'real_property_id' => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('RealProperty'), 'column' => 'id')),
       'created_at'       => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
       'updated_at'       => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
@@ -48,6 +50,7 @@ abstract class BaseGalleryFormFilter extends BaseFormFilterDoctrine
       'id'               => 'Number',
       'former_name'      => 'Text',
       'internal_name'    => 'Text',
+      'outstanding'      => 'Boolean',
       'real_property_id' => 'ForeignKey',
       'created_at'       => 'Date',
       'updated_at'       => 'Date',
