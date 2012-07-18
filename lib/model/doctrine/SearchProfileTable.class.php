4425f56939b9268eb699f3a461d74f8f22796f00
elio <elio@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-07-18 17:40:49 +0000

    ELIO: upd search profile restriction

--- lib/model/doctrine/SearchProfileTable.class.php
+++ lib/model/doctrine/SearchProfileTable.class.php
@@ -51,5 +51,45 @@ class SearchProfileTable extends Doctrine_Table
 
 		return $q->execute();
   }
+  
+  /**
+   * Check if search is already saved
+   *
+   * @param object $u
+   * @return boolean
+   */
+  public function checkSearchAlreadySaved($u)
+  {
+  	$r = false;
+
+  	if ($u->isAuthenticated()) {
+  		$f = 'app_user_id = '.$u->getAttribute('user_id');
+
+  		$_bedroom       = $u->getAttribute('sch_bedroom');
+  		$_property_type = $u->getAttribute('sch_property_type');
+  		$_operation     = $u->getAttribute('sch_operation');
+  		$_geo_zone      = $u->getAttribute('sch_geo_zone');
+  		$_city          = $u->getAttribute('sch_city');
+  		$_neighborhood  = $u->getAttribute('sch_neighborhood');
+  		$_currency      = $u->getAttribute('sch_currency');
+  		$_p_from        = $u->getAttribute('sch_p_from');
+  		$_p_to          = $u->getAttribute('sch_p_to');
+
+  		if (empty($_bedroom))       { $f .= ' AND bedroom_id IS NULL'; }       else { $f .= " AND bedroom_id = $_bedroom"; }
+  		if (empty($_property_type)) { $f .= ' AND property_type_id IS NULL'; } else { $f .= " AND property_type_id = $_property_type"; }
+  		if (empty($_operation))     { $f .= ' AND operation_id IS NULL'; }     else { $f .= " AND operation_id = $_operation"; }
+  		if (empty($_geo_zone))      { $f .= ' AND geo_zone_id IS NULL'; }      else { $f .= " AND geo_zone_id = $_geo_zone"; }
+  		if (empty($_city))          { $f .= ' AND city_id IS NULL'; }          else { $f .= " AND city_id = $_city"; }
+  		if (empty($_neighborhood))  { $f .= ' AND neighborhood_id IS NULL'; }  else { $f .= " AND neighborhood_id = $_neighborhood"; }
+  		if (empty($_currency))      { $f .= ' AND currency_id IS NULL'; }      else { $f .= " AND currency_id = $_currency"; }
+  		if (empty($_p_from))        { $f .= ' AND min_price = 0'; }            else { $f .= " AND min_price = $_p_from"; }
+  		if (empty($_p_to))          { $f .= ' AND max_price = 0'; }            else { $f .= " AND max_price = $_p_to"; }
+
+  		$q = Doctrine_Query::create()->from('SearchProfile')->where($f)->fetchOne();
+
+  		if (!$q) { $r = true; }
+  	}
+  	return $r;
+  }
 
 } // end class
