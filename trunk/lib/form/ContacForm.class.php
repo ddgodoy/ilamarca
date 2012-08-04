f1048c0877538c0c0a4da624ca8a9c663893488b
mauro2 <mauro2@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-08-04 13:11:35 +0000

    new schema table email share

--- trunk/lib/form/ContacForm.class.php
+++ trunk/lib/form/ContacForm.class.php
@@ -10,13 +10,19 @@
 
 class ContacForm extends sfForm
 {
+  private $requets = '';
   public function configure()
   {
+    $this->requets = sfContext::getInstance()->getRequest();
+
+    $type = $this->requets->getParameter('type', '');
+
     $this->setWidgets(array(
       'name'        => new sfWidgetFormInput(array()),
       'email'       => new sfWidgetFormInput(array()),
       'phone'       => new sfWidgetFormInput(array()),
-      'address'       => new sfWidgetFormInput(array()),
+      'address'     => new sfWidgetFormInput(array()),
+      'type'        => new sfWidgetFormInputHidden(array()),
       'message'     => new sfWidgetFormTextarea()
     ));
     
@@ -25,13 +31,14 @@ class ContacForm extends sfForm
       'email'       => new sfValidatorAnd(array(new sfValidatorEmail(array(),array('invalid'=>'Enter valid Email Address'))),array(),array('required' => 'this field is mandatory')),
       'phone'     => new sfValidatorString(array('max_length' => 150, 'required' => true), array('required' => 'this field is mandatory')),
       'address'     => new sfValidatorString(array('max_length' => 150, 'required' => FALSE), array('required' => 'this field is mandatory')),
-      'message'     => new sfValidatorString(array('required' => true))
-
+      'message'     => new sfValidatorString(array('required' => true)),
+      'type'        => new sfValidatorString(array('required' => FALSE)),
     ));
 
 
     $this->widgetSchema->setNameFormat('contac[%s]');
 
+    $this->setDefault('type', $type);
 
   }
 }
---
+++ trunk/lib/form/doctrine/base/BaseEmailShareForm.class.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * EmailShare form base class.
+ *
+ * @method EmailShare getObject() Returns the current form's model object
+ *
+ * @package    sf_icox
+ * @subpackage form
+ * @author     pinika
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
+ */
+abstract class BaseEmailShareForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'           => new sfWidgetFormInputHidden(),
+      'name'         => new sfWidgetFormInputText(),
+      'email'        => new sfWidgetFormInputText(),
+      'email_friend' => new sfWidgetFormInputText(),
+      'comment'      => new sfWidgetFormInputText(),
+      'created_at'   => new sfWidgetFormDateTime(),
+      'updated_at'   => new sfWidgetFormDateTime(),
+    ));
+
+    $this->setValidators(array(
+      'id'           => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'name'         => new sfValidatorString(array('max_length' => 250)),
+      'email'        => new sfValidatorString(array('max_length' => 250)),
+      'email_friend' => new sfValidatorString(array('max_length' => 250)),
+      'comment'      => new sfValidatorPass(array('required' => false)),
+      'created_at'   => new sfValidatorDateTime(),
+      'updated_at'   => new sfValidatorDateTime(),
+    ));
+
+    $this->widgetSchema->setNameFormat('email_share[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'EmailShare';
+  }
+
+}
