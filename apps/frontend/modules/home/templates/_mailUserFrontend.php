de2d30c36485bfb4abc6f789f49c1c021cc33603
elio <elio@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-08-22 13:57:58 +0000

    ELIO: add last upd

--- apps/frontend/modules/home/templates/_mailUserFrontend.php
+++ apps/frontend/modules/home/templates/_mailUserFrontend.php
@@ -2,11 +2,10 @@
 	<head>
 		<title>Activación de cuenta</title>
 	</head>
+	<p>
+		Debes confirmar tu registro completando un último paso según se indica a continuación.
+	</p>
 	<table>
-		<tr>
-			<td colspan="2">Debes confirmar tu registro completando un último paso según se indica a continuación.<td>
-		</tr>
-		<tr><td height="10"></td></tr>
 		<tr>
 			<td><strong>Para habilitar tu cuenta:</strong>&nbsp;<td>
       <td><a href="<?php echo url_for('user/activateAccount?tk='.$data['token'], true) ?>">click aquí</a></td>
--- lib/model/doctrine/VendorZoneTable.class.php
+++ lib/model/doctrine/VendorZoneTable.class.php
@@ -1,11 +1,32 @@
 <?php
 
-
 class VendorZoneTable extends Doctrine_Table
-{
-    
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('VendorZone');
-    }
-}
+{    
+  public static function getInstance() { return Doctrine_Core::getTable('VendorZone'); }
+  
+  /**
+   * Ger zone vendor for this property
+   *
+   * @param integer $neighborhood_id
+   * @param integer $owner_id
+   * @return array
+   */
+  public function getZoneVendorsForContact($neighborhood_id, $owner_id)
+  {
+  	$a = array();
+  	$q = Doctrine_Query::create()
+  			 ->from('VendorZone vz')
+  			 ->leftJoin('vz.AppUser au')
+  			 ->where("vz.neighborhood_id = $neighborhood_id AND vz.app_user_id != $owner_id");
+  	
+  	if ($q->count() > 0) {
+  		$d = $q->execute();
+  		
+  		foreach ($d as $value) {
+  			$a[$value->AppUser->getEmail()] = $value->AppUser->getName().' '.$value->AppUser->getLastName();
+  		}
+  	}
+  	return $a;
+  }
+  
+} // end class
