7024f3566c61f9004888ef0283425f984137def7
elio <elio@8810f466-b969-4c55-8445-cd4e421a3e2c>
2012-09-10 14:06:16 +0000

    ELIO: add banner to classic site

--- apps/frontend/modules/home/templates/_rateProperty.php
+++ apps/frontend/modules/home/templates/_rateProperty.php
@@ -1,14 +1,19 @@
+<map name="to_old_site">
+  <area shape="rect" coords="174,4,294,55" href="http://clasico.ilamarca.com/" alt="iLamarca" />
+</map>
+<img src="/images/banner_chico_02.png" border="0" usemap="#to_old_site" />
+<br /><br />
 <div class="box clearfix">
-        <div class="inner clearfix">
-                <div class="titulo"><img src="/images/tit_tasa.png" alt="Tasá tu propiedad" /></div>
-                <p>
-                        ¿Querés vender o alquilar tu casa?<br />
-                        Te brindamos la seguridad de un <strong>Perito Tasador Oficial.</strong>
-                </p>
-                <div class="boton">
-                  <a href="<?php echo url_for('home/contact?type=vende') ?>" class="vende"></a>
-                  <a href="<?php echo url_for('home/contact?type=alquila') ?>" class="alquila"></a>
-                </div>
-        </div>
+  <div class="inner clearfix">
+    <div class="titulo"><img src="/images/tit_tasa.png" alt="Tasá tu propiedad" /></div>
+    <p>
+      ¿Querés vender o alquilar tu casa?<br />
+      Te brindamos la seguridad de un <strong>Perito Tasador Oficial.</strong>
+    </p>
+    <div class="boton">
+      <a href="<?php echo url_for('home/contact?type=vende') ?>" class="vende"></a>
+      <a href="<?php echo url_for('home/contact?type=alquila') ?>" class="alquila"></a>
+    </div>
+  </div>
 </div>
 <div class="sombra"></div>
