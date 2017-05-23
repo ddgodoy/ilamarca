4ffc001c4914974dfc28b847900ec924fa5a5e58
leonard.gonzalez <leonard.gonzalez@icox.com>
2017-05-23 13:10:48 +0000

    fixed

--- apps/frontend/modules/property/templates/indexSuccess.php
+++ apps/frontend/modules/property/templates/indexSuccess.php
@@ -240,9 +240,8 @@
                     <a href="http://www.facebook.com/sharer.php?u=<?php echo $url_site ?>" class="share-fb" target="_blanck"></a>
                     <a class="share-email" id="" title="<?php echo truncate_text($property->getName(), 55, '') ?>"
                        href="mailto:?subject=I wanted you to see this site&amp;body=<?php echo $url_site.' '; echo truncate_text($property->getName(), 55, ''); ?>" ></a>
-                    <a href="whatsapp://send" class="share-whatsapp"
-                       data-text="<?php echo truncate_text($property->getName(), 55, ''); ?>"
-                       data-href=""></a>
+                    <a href="whatsapp://send?text=<?php echo $url_site; ?>" class="share-whatsapp"
+                       data-action="share/whatsapp/share"></a>
                 <?php endif; ?>
 
             </div>
